486f53fb9dbbf5b0288d7af9d57bc6616926024f
ana <anate82@gmail.com>
2021-03-11 17:04:49 +0000

    employees by query

--- api/routes/employees.router.js
+++ api/routes/employees.router.js
@@ -5,12 +5,13 @@ const {
   createEmployee,
   getEmployees,
   getemployeeById,
+  getEmployeesByQuery,
   updateEmployeeById,
 } = require('../controllers/employees.controller')
 
 router.get('/', getEmployees)
+router.get('/search', getEmployeesByQuery)
 router.get('/:employeeId', getemployeeById)
-
 router.post('/', createEmployee)
 router.put('/:employeeId', updateEmployeeById)
 
