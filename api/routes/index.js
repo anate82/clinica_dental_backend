6c68dc17c67a16180df8657e3110f3fb6aa6874c
ana <anate82@gmail.com>
2021-03-17 11:59:19 +0000

    add status server

--- api/routes/index.js
+++ api/routes/index.js
@@ -9,6 +9,11 @@ router.use('/auth', authRouter)
 router.use('/appointments', appointmentRouter)
 router.use('/employees', employeesRouter)
 router.use('/patients', patientsRouter)
+router.use('/status', status)
 router.use('/treatments', treatmentRouter)
+
+function status(req, res) {
+  res.status(200).json('Server is running')
+}
 
 module.exports = router
